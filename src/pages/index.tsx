663427511db13c1e9a03116603141e3e4a7a962f
Efraim Andrade <efraim.dev@gmail.com>
2021-04-23 09:00:27 -0300

    Estrutura do app - ...configurando fonte externa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,9 @@
+import styles from "../styles/home.module.scss";
+
 export default function Home() {
-  return <h1>hello world</h1>;
+  return (
+    <h1 className={styles.title}>
+      hello <span>world</span>
+    </h1>
+  );
 }
