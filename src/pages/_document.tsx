4871066f6597f2ddcbca0741e0ada29839e37834
Efraim Andrade <efraim.dev@gmail.com>
2021-04-25 11:36:48 -0300

    Componentes e paginas: ...componente: SubscribeButton

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <title>ig.news</title>
-
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,35 @@
-import styles from "../styles/home.module.scss";
+import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      hello <span>world</span>
-    </h1>
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+
+          <h1>
+            News about <br />
+            the <span>React</span> world.
+          </h1>
+
+          <p>
+            Get access to all the publications
+            <br />
+            <span>for $9.90 month</span>
+          </p>
+
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
   );
 }
